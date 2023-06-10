b1995334b505b6f238228f5a315b2bf3a7f7c917
Wonwoo Choi <vbchunguk@bacchus.snucse.org>
2023-06-10 10:43:01 +0900

    그룹 관리자 탭 스타일링

--- app/group/[id]/NavigationTab.tsx
+++ app/group/[id]/NavigationTab.tsx
@@ -5,26 +5,35 @@ import { useParams, usePathname } from 'next/navigation';
 
 export default function NavigationTab() {
   const { id } = useParams();
-  const pathname = usePathname();
 
   return (
-    <ul className="text-lg flex flex-row gap-2">
-      <li className="flex-1 text-center">
-        <Link
-          className={pathname.endsWith('members') ? 'font-bold' : ''}
-          href={`/group/${id}/members`}
-        >
-          멤버 목록
-        </Link>
-      </li>
-      <li className="flex-1 text-center">
-        <Link
-          className={pathname.endsWith('pending') ? 'font-bold' : ''}
-          href={`/group/${id}/pending`}
-        >
-          신청 대기 목록
-        </Link>
-      </li>
-    </ul>
+    <div className="text-lg flex flex-row text-center border-b border-black/10 dark:border-white/10">
+      <Tab href={`/group/${id}/members`}>
+        멤버 목록
+      </Tab>
+      <Tab href={`/group/${id}/pending`}>
+        신청 대기 목록
+      </Tab>
+    </div>
+  );
+}
+
+function Tab(props: { href: string; children: React.ReactNode }) {
+  const { href, children } = props;
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
+  let className = 'flex-1 rounded-t p-2 transition';
+  if (isActive) {
+    className += ' font-bold border-b-2 border-primary-300 dark:border-primary-600'
+      + ' bg-black/20 dark:bg-white/20';
+  } else {
+    className += ' hover:bg-black/10 dark:hover:bg-white/10';
+  }
+
+  return (
+    <Link className={className} href={href}>
+      {children}
+    </Link>
   );
 }
