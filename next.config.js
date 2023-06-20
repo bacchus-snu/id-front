1fd2e06e831d33f83b703071dc3b63a7ef6f7f43
Wonwoo Choi <vbchunguk@bacchus.snucse.org>
2023-06-20 20:49:43 +0900

    구 프론트엔드 주소 리디렉션 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/sign-up',
+        destination: '/signup',
+        permanent: false,
+      },
+      {
+        source: '/verify',
+        destination: '/signup',
+        permanent: false,
+      },
+      {
+        source: '/my-page',
+        destination: '/',
+        permanent: false,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
