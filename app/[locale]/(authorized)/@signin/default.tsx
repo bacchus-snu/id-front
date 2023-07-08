f828e2d69aa300c0b5e119c5490ba5dfbc82676b
Wonwoo Choi <vbchunguk@bacchus.snucse.org>
2023-07-09 00:25:41 +0900

    문자열 추출 추가

--- app/[locale]/(authorized)/@signin/default.tsx
+++ app/[locale]/(authorized)/@signin/default.tsx
@@ -1,18 +1,36 @@
 import Link from 'next/link';
 
+import { getDictionary, Locale } from '@/locale';
+
 import SignInForm from './SignInForm';
 
-export default function Login() {
+type Props = {
+  params: { locale: Locale };
+};
+
+export default async function SignIn({
+  params: { locale },
+}: Props) {
+  const dict = await getDictionary(locale);
+  const signUpNodes: React.ReactNode[] = dict.signIn.signUpLabel.split('{}');
+  const changePasswordNodes: React.ReactNode[] = dict.signIn.changePasswordLabel.split('{}');
+  signUpNodes.splice(
+    1,
+    0,
+    <Link key="link" className="text-link" href="/signup">{dict.links.signUp}</Link>,
+  );
+  changePasswordNodes.splice(
+    1,
+    0,
+    <Link className="text-link" href="/password">{dict.links.changePassword}</Link>,
+  );
+
   return (
     <>
       <SignInForm />
       <section className="text-right w-full max-w-xs mx-auto mt-2">
-        <div>
-          계정이 없으신가요? <Link className="text-link" href="/signup">가입하기</Link>
-        </div>
-        <div>
-          비밀번호를 잊으셨나요? <Link className="text-link" href="/password">비밀번호 변경</Link>
-        </div>
+        <div>{signUpNodes}</div>
+        <div>{changePasswordNodes}</div>
       </section>
     </>
   );
