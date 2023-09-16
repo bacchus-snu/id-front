0d3ec158aa20ad22a4109eff2d5f1eece0d615ee
Wonwoo Choi <vbchunguk@bacchus.snucse.org>
2023-09-16 15:08:47 +0900

    /oauth를 다시 nolocale 그룹으로 이동 (#6)

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -5,10 +5,9 @@ import { getDictionary, Locale } from '@/locale';
 
 type Props = {
   locale: Locale;
-  oauthUid?: string;
 };
 
-export default async function SignIn({ locale, oauthUid }: Props) {
+export default async function SignIn({ locale }: Props) {
   const dict = await getDictionary(locale);
   const signUpNodes: React.ReactNode[] = dict.signIn.signUpLabel.split('{}');
   const changePasswordNodes: React.ReactNode[] = dict.signIn.changePasswordLabel.split('{}');
@@ -25,7 +24,7 @@ export default async function SignIn({ locale, oauthUid }: Props) {
 
   return (
     <>
-      <SignInForm oauthUid={oauthUid} />
+      <SignInForm />
       <section className="text-right w-full max-w-xs mx-auto mt-2">
         <div>{signUpNodes}</div>
         <div>{changePasswordNodes}</div>
