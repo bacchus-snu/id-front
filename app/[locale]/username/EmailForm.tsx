9b1f97f2e57fe62b887613998d460bde43fd6b86
Chanyeong Lim <asp345@posteo.net>
2026-04-09 13:04:19 +0900

    feat: 유저명(id) 찾기 기능 추가 (#19)
    * feat: find username page
    
    * fix: english locale of findUsername
    
    * fix: add /username/email to middleware bypass and fix 429 status code
    
    ---------
    
    Co-authored-by: Woohyeon Baek <whnbaek@gmail.com>

---
+++ app/[locale]/username/EmailForm.tsx
@@ -0,0 +1,103 @@
+'use client';
+
+import { useState } from 'react';
+
+import Button from '@/components/Button';
+import useLocaleDict from '@/components/LocaleDict';
+import { useToast } from '@/components/NotificationContext';
+
+enum RequestState {
+  Idle,
+  Pending,
+  Done,
+}
+export default function EmailForm() {
+  const { dict } = useLocaleDict();
+  const usernameEmailDict = dict.findUsername.email;
+  const showToast = useToast();
+
+  const [email, setEmail] = useState('');
+  const [requestState, setRequestState] = useState(RequestState.Idle);
+  const [valid, setValid] = useState(false);
+
+  function handleChangeEmail(e: React.ChangeEvent<HTMLInputElement>) {
+    setEmail(e.target.value);
+    setValid(e.target.checkValidity());
+  }
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    if (!valid) {
+      return;
+    }
+
+    try {
+      setRequestState(RequestState.Pending);
+      const resp = await fetch('/username/email', {
+        method: 'post',
+        body: JSON.stringify({ email }),
+        headers: {
+          'content-type': 'application/json',
+        },
+      });
+
+      if (!resp.ok) {
+        const body = await resp.json();
+        const message = body?.message;
+        if (message) {
+          showToast({
+            type: 'error',
+            message,
+          });
+        }
+        return;
+      }
+    } catch (e) {
+      console.error(e);
+      showToast({
+        type: 'error',
+        message: dict.error.unknown,
+      });
+      return;
+    } finally {
+      setRequestState(RequestState.Idle);
+    }
+
+    setRequestState(RequestState.Done);
+  }
+
+  if (requestState === RequestState.Done) {
+    return (
+      <section className="border rounded p-2">
+        <h2 className="text-h2 mb-2">{usernameEmailDict.emailSent.smallTitle}</h2>
+        {usernameEmailDict.emailSent.descriptions.map((desc, index) => (
+          <p key={index} dangerouslySetInnerHTML={{ __html: desc }} />
+        ))}
+      </section>
+    );
+  }
+
+  return (
+    <section className="border rounded p-2">
+      <h2 className="text-h2 mb-2">{dict.title.findUsername}</h2>
+      <p>{usernameEmailDict.description}</p>
+      <form className="flex flex-row flex-wrap justify-end gap-2 mt-2" onSubmit={handleSubmit}>
+        <input
+          className="w-full flex-none sm:flex-1 bg-transparent border rounded p-1"
+          type="email"
+          required
+          value={email}
+          onChange={handleChangeEmail}
+        />
+        <Button
+          className="flex-0 w-32 font-bold"
+          color="primary"
+          type="submit"
+          disabled={!valid || requestState !== RequestState.Idle}
+        >
+          {usernameEmailDict.buttonSendEmail}
+        </Button>
+      </form>
+    </section>
+  );
+}
